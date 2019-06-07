fd5a92dd69094fe1482d5569bb4a11fb5224bfa7
mohit1604 <malikmohit619@gmail.com>
2019-06-07 15:57:38 +0530

    searchInput Component update-1

--- src/Components/SearchInput.js
+++ src/Components/SearchInput.js
@@ -1,67 +1,77 @@
 import React, { Component } from "react";
+import Select from "react-select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faImage } from "@fortawesome/free-solid-svg-icons";
-// import { faSearch } from "@fortawesome/free-solid-svg-icons";
-// import { faImage } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faFileAlt,
+  faCamera,
+  faVideo
+} from "@fortawesome/free-solid-svg-icons";
+
+const options = [
+  { value: "text", label: <FontAwesomeIcon icon={faFileAlt} /> },
+  { value: "image", label: <FontAwesomeIcon icon={faCamera} /> },
+  { value: "video", label: <FontAwesomeIcon icon={faVideo} /> }
+];
+
 class SearchInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectValue: "text"
+      selectedOption: "image"
     };
   }
 
-  onSelection(e) {
-    this.setState({
-      selectValue: e.target.value
-    });
-    console.log(this.state.selectValue);
+  handleChange(e) {
+    console.log(e);
+    this.setState({ selectedOption: e.value });
+    console.log(`Option selected:`, this.state.selectedOption);
   }
 
   render() {
-    console.log(this.state.selectValue);
+    const { selectedOption } = this.state;
+
     return (
-      <div className="container">
-        <form className="form-inline">
-          <select
-            className="form-control mr-2"
-            id="content-type"
-            onChange={this.onSelection.bind(this)}
-          >
-            <option value="text">
-              Text
-              <FontAwesomeIcon icon={faSearch} />
-            </option>
-            <option value="image">Image &#xf1c8;</option>
-            <option value="video">
-              Video
-              <FontAwesomeIcon icon={faImage} />
-            </option>
-          </select>
-          <div className="search-content">
-            <input
-              className="form-control mr-2"
-              type="text"
-              id="search-input"
-              placeholder="Search"
-            />
-            <button className="btn" type="button">
-              <FontAwesomeIcon icon={faSearch} />
-            </button>
-          </div>
-          <div className="custom-file search-content">
-            <input className="custom-file-input" type="file" id="myfile" />
-            <label className="custom-file-label" htmlFor="myfile">
-              Upload Image
-            </label>
-          </div>
-          <div className="custom-file search-content">
-            <input className="custom-file-input" type="file" id="myfile" />
-            <label className="custom-file-label" htmlFor="myfile">
-              Upload Video
-            </label>
-          </div>
+      <div className="container search-box mx-auto mt-5">
+        <form className="form-inline search-form">
+          <Select
+            className="mr-2"
+            placeholder={<FontAwesomeIcon icon={faFileAlt} />}
+            value={selectedOption}
+            onChange={this.handleChange.bind(this)}
+            options={options}
+          />
+          {this.state.selectedOption === "text" ? (
+            <div className="search-content">
+              <input
+                className="form-control mr-2"
+                type="text"
+                id="search-input"
+                placeholder="Search"
+              />
+            </div>
+          ) : this.state.selectedOption === "image" ? (
+            <div className="search-content">
+              <div className="custom-file  search-content">
+                <input className="custom-file-input" type="file" id="myfile" />
+                <label className="custom-file-label p-auto" htmlFor="myfile">
+                  Upload Image
+                </label>
+              </div>
+            </div>
+          ) : (
+            <div className="search-content">
+              <div className="custom-file ">
+                <input className="custom-file-input" type="file" id="myfile" />
+                <label className="custom-file-label" htmlFor="myfile">
+                  Upload Video
+                </label>
+              </div>
+            </div>
+          )}
+          <button className="btn search-btn ml-2" type="button">
+            <FontAwesomeIcon icon={faSearch} color="#fff" />
+          </button>
         </form>
       </div>
     );
