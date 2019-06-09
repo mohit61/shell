8d715dfceb6801333f66d2ed5347ec443f582d36
mohit1604 <malikmohit619@gmail.com>
2019-06-09 17:23:19 +0530

    fixing header for small screen

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "../../src/logo_logomark.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faHome } from "@fortawesome/free-solid-svg-icons";
 
 class Header extends Component {
   render() {
@@ -23,13 +23,26 @@ class Header extends Component {
                   Search &raquo;
                 </a>
               </li>
-              <li className="active nav-item">
-                <a className="nav-link">abcc</a>
+              <li className="nav-item nav-link">
+                <a className="active">abcc</a>
               </li>
             </ul>
             <a className="nav-link">
               <FontAwesomeIcon icon={faUser} color="#ea6565" />
             </a>
+          </div>
+        </nav>
+        <nav className="navbar d-lg-none d-md-none d-sm-none justify-content-center">
+          <div className="px-2 nav-link">
+            <a>
+              <FontAwesomeIcon icon={faHome} color="#ea6565" /> &raquo;
+            </a>
+          </div>
+          <div className="px-2 nav-link">
+            <a>Search &raquo;</a>
+          </div>
+          <div className="px-2 nav-link ">
+            <a className="active">abcc</a>
           </div>
         </nav>
       </div>
--- src/ReusableComponents/SubmitFormButton.js
+++ src/ReusableComponents/SubmitFormButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SubmitFormButton = props => {
   return (
-    <button type="submit" className="btn btn-block mt-4">
+    <button type="submit" className="btn btn-submit btn-block mt-4">
       {props.submit_button_text}
     </button>
   );
