be34e7a44c986c3229b518267bb7f16da3d5ba00
mohit1604 <malikmohit619@gmail.com>
2019-06-10 16:01:55 +0530

    redux initial setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 //Components
 import Login from "./Components/Login";
@@ -16,19 +18,21 @@ import Home from "./Components/Home";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/cards" component={Cards} />
-        <Route exact path="/card/:id" component={CardDetails} />
-        <Route exact path="/header" component={Header} />
-        <Route exact path="/footer" component={FooterSite} />
-        <Route exact path="/search" component={SearchInput} />
-        <Route exact path="/infoPanel" component={InfoPanel} />
-        <Route exact path="/sidebar" component={Sidebar} />
-        <Route exact path="/home" component={Home} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/cards" component={Cards} />
+          <Route exact path="/card/:id" component={CardDetails} />
+          <Route exact path="/header" component={Header} />
+          <Route exact path="/footer" component={FooterSite} />
+          <Route exact path="/search" component={SearchInput} />
+          <Route exact path="/infoPanel" component={InfoPanel} />
+          <Route exact path="/sidebar" component={Sidebar} />
+          <Route exact path="/home" component={Home} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
